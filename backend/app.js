855cfe90fa2038e2019eaca447b8f4c3dfa9ffd5
kamal0029 <kamala.20it@kongu.edu>
2023-05-20 16:22:51 +0530

    build

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,7 @@ app.use('/api/v1/',payment);
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '../frontend/build')));
+    console.log(__dirname);
     app.get('*', (req, res) =>{
         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
     })
